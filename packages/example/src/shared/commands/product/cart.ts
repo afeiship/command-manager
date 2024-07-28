cbcaf3d9ffa896449471f77e689b1374cc8add87
afeiship <1290657123@qq.com>
2024-07-28 21:53:46 +0800

    wip

---
+++ packages/example/src/shared/commands/product/cart.ts
@@ -0,0 +1,16 @@
+import { defineCommand } from '@jswork/react-command-manager/src/main';
+
+/**
+ * 如果定义了名字，直接用 name, 而不是用 path 的变化名字
+ */
+export default defineCommand({
+  name: 'cart',
+  methods: {
+    init() {
+      console.log('init cart');
+    },
+    add2cart(){
+      console.log('add to cart.');
+    }
+  },
+});
