e7e0f59b881442a1e54612bff30b18626b92de4b
afeiship <1290657123@qq.com>
2024-08-01 22:53:57 +0800

    chore: export type

--- packages/lib/src/main.tsx
+++ packages/lib/src/main.tsx
@@ -2,9 +2,9 @@ import ReactCommandManager from './react';
 import CommandManager, { CommandManagerOptions } from './command-manager';
 import defineCommand, { DefineCommandResult, DefineCommand, DefineCommandOptions } from './define-command';
 
+export type { CommandManagerOptions, DefineCommandResult, DefineCommand, DefineCommandOptions };
+
 export {
   // widgets
   ReactCommandManager, defineCommand, CommandManager,
-  // types
-  DefineCommandResult, DefineCommand, DefineCommandOptions, CommandManagerOptions,
 };
