06df8f43a8b981595884f35ac4ebaae84f033ecd
afeiship <1290657123@qq.com>
2024-08-02 21:16:47 +0800

    test: versio

--- packages/command-manager/src/index.ts
+++ packages/command-manager/src/index.ts
@@ -1,7 +1,7 @@
 import CommandManager, { CommandManagerOptions } from './command-manager';
 import defineCommand, { DefineCommandResult, DefineCommand, DefineCommandOptions } from './define-command';
 
-// export types - for consumers of the package
+// export types
 export type { CommandManagerOptions, DefineCommandResult, DefineCommand, DefineCommandOptions };
 
 // export modules
