d3e2838edd7805abc54746707629b4b53de3fc02
afeiship <1290657123@qq.com>
2024-08-02 21:00:14 +0800

    chore: upt types

--- packages/command-manager/src/index.ts
+++ packages/command-manager/src/index.ts
@@ -1,6 +1,8 @@
 import CommandManager, { CommandManagerOptions } from './command-manager';
 import defineCommand, { DefineCommandResult, DefineCommand, DefineCommandOptions } from './define-command';
 
+// export types
 export type { CommandManagerOptions, DefineCommandResult, DefineCommand, DefineCommandOptions };
 
+// export modules
 export { CommandManager, defineCommand };
